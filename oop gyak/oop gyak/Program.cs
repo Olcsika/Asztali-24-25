c4dcb721f12af2d401da3da15173d5cfa2a0a282
Lébár Olivér <lebaro21@ady-nagyatad.sulinet.hu>
2024-09-13 14:18:31 +0200

    a

---
+++ oop gyak/oop gyak/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using oop_gyak;
+using System.Text.Json.Serialization;
+
+Console.WriteLine("Hello, World!");
+kutya k = new kutya("Papucs");
+k.ugat();
+k.suly= -12;
+Console.WriteLine(k.suly);
+Console.WriteLine(k);
+
+Console.WriteLine(kutya.ezMiEz());
---
+++ oop gyak/oop gyak/kutya.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_gyak
+{
+    internal class kutya
+    {
+
+        public string nev;
+
+        public kutya(string nev)
+            
+        {
+            this.nev = nev;
+            Console.WriteLine("Kutya megszületett");
+        }
+        public void ugat()
+        {
+            Console.WriteLine("vauuuuu");
+        }
+        private float _suly;
+        public float suly
+        {
+            get
+            {
+                return _suly;
+            }
+            set
+            {
+                _suly = Math.Abs(value) ;
+            }
+        }
+        public override string ToString()
+        {
+            return "Kutya: "+ nev+ "Suly: "+_suly ;
+        }
+        static public string  ezMiEz()
+        {
+            return "ez egy kutya";
+        }
+        
+    }
+}
