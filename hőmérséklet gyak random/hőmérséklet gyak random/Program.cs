13b5b8c891d974d89d596ed4b5ad77dbd2d932c0
Lébár Olivér <lebaro21@ady-nagyatad.sulinet.hu>
2024-09-20 09:37:27 +0200

    a

---
+++ hőmérséklet gyak random/hőmérséklet gyak random/Program.cs
@@ -0,0 +1,12 @@
+﻿//nap 24 orajaban random c fok, +/- 5 fok, este ejszaka csak csokkenhet,
+//nappal csak nőhet, min 10, max 40
+Console.WriteLine("Hello, World!");
+
+var nap = 0;
+
+Random rnd = new Random();
+for (int i = 0; i < 25; i++)
+{
+    Console.WriteLine(i +" "+rnd.Next(10,40));
+}
+
