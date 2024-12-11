21aec3dca823d5f616f1ae98a26fdff2e863b342
Lébár Olivér <lebaro21@ady-nagyatad.sulinet.hu>
2024-12-11 13:29:19 +0100

    a

---
+++ gyakszi 12.11/gyakszi 12.11/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Text.RegularExpressions;
+
+string text = File.ReadAllText("alma.txt");
+
+Regex minta = new Regex(@"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})");
+var talalat = minta.Matches(text);
+Console.WriteLine(talalat.Count);
